9b1b29886ac420f2a3eb6cacf16fa56598fda8f2
Laith <info@laithazzam.com>
2016-07-31 22:50:24 -0700

    initial trade tracker

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+'use strict';
+
+var gulp        = require('gulp');
+var runSequence = require('run-sequence');
+var requireDir  = require('require-dir');
+
+
+
+// Require all tasks.
+requireDir('./gulp/tasks', { recurse: true });
+
+
+// Commonly used tasks defined here.
+gulp.task('default', function() {
+	runSequence(
+		'clean',
+		[
+			'jade',
+			'sass',
+			'scripts',
+			'images',
+			'favicon'
+		],
+		'watch',
+		'connect'
+	);
+});
+
+
+
+// Build task.
+gulp.task('build', function() {
+	runSequence(
+		'clean',
+		[
+			'build-images',
+			'build-scripts',
+			'build-css',
+			'build-fonts',
+			'jade'
+		],
+		'build-html',
+		'connect'
+	);
+});
